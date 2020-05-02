63550e64f6bfff7832d2ee43290592a7f3077cf5
PiotrNap <piotr.napierala94@gmail.com>
2020-05-02 15:37:47 +0200

    Initial commit

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = "Error connection to database";
+
+  constructor() {
+    super("Error connection to database");
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
